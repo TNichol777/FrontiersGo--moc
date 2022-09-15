61e261f08a309c1f17d15188532eafc56fccaa23
Trey Nicholson <tnichol777@gmail.com>
2022-09-15 14:30:16 -0700

    Second update

---
+++ app.js
@@ -0,0 +1,28 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-menu');
+    const navLinks = document.querySelectorAll('.main-menu li');
+
+
+
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation =
+                    `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
+            }
+        });
+
+        //burger animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
